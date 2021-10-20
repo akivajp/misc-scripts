650d32010dfb4c50b3ae9a75c96fe215ae95806e
Akiva Miura <akiva.miura@gmail.com>
2021-10-20 18:26:46 +0900

    Added depth option for zfs-purge-snapshot.sh

--- zfs-purge-snapshot.sh
+++ zfs-purge-snapshot.sh
@@ -1,21 +1,51 @@
-#!/bin/sh
+#!/bin/bash
 
 PATH=$PATH:/sbin:/usr/sbin
 
-ZFS_list_snap='zfs list -H -o name -d 1 -t snapshot'
+args=()
 
-if [ $# -le 0 ]; then
-    echo "Usage: ${0##*/} zfs [zfs ...]" 1>&2
-    exit 2
+depth=1
+error=""
+while (( $# > 0 ))
+do
+    case $1 in
+        -d | --depth)
+            if [[ -z "$2" ]]; then
+                echo "-d|--depth requires an argument" > /dev/stderr
+                exit 1
+            fi
+            depth=$2
+            shift
+            ;;
+        --depth=*)
+            depth=$(echo $1 | sed -e 's/^--depth=//')
+            ;;
+        *)
+            args+=($1)
+    esac
+    shift
+done
+
+if [ ${#args} -lt 1 ]; then
+    echo "Usage: ${0##*/} [-d|--depth depth] zfs [zfs ...]" 1>&2
+    exit 1
 fi
 
-for fs in "$@"; do
-    if [ $(${ZFS_list_snap} "${fs}" | wc -l) = 0 ]; then
-            continue
+echo "depth: ${depth}"
+echo "args: ${args[@]}"
+
+ZFS_list_snap="zfs list -H -o name -d ${depth} -t snapshot"
+
+for fs in "${args[@]}"; do
+    echo "[exec] ${ZFS_list_snap} ${fs}"
+    snapshots=($(${ZFS_list_snap} "${fs}"))
+    if [ ${#snapshots} -lt 1 ]; then
+        echo "no snapshots are found"
+    else
+        for snapshot in "${snapshots[@]}"; do
+            echo "[exec] zfs destroy ${snapshot} || exit"
+            zfs destroy ${snapshot} || exit
+        done
     fi
-    snap0=$(${ZFS_list_snap} "${fs}" | head -1)
-    snap1=$(${ZFS_list_snap} "${fs}" | tail -1 | sed 's/.*@//')
-    echo "[exec] zfs destroy "${snap0}%${snap1}" || exit"
-    zfs destroy "${snap0}%${snap1}" || exit
 done
 
